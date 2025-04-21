45ffd3ed070a3646106ac7374171a697ba7e2a00
BichuPg <115611237+BichuPg@users.noreply.github.com>
2025-04-21 10:51:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/Yy2.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/5d76a1d0cd42f5c20fc58.jpg')).split()
 
 
 # Admins & Users
