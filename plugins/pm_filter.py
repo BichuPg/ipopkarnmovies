808fceef66a8b4e71743842c0f4b98427ea12f93
BichuPg <115611237+BichuPg@users.noreply.github.com>
2025-04-30 20:35:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1363,29 +1363,29 @@ async def cb_handler(client: Client, query: CallbackQuery):
             if settings['is_shortlink'] and not await db.has_premium_access(query.from_user.id):
                 if clicked == typed:
                     temp.SHORT[clicked] = query.message.chat.id
-                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichushort_{file_id}")
+                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=short_{file_id}")
                     return
                 else:
                     await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
             elif settings['is_shortlink'] and await db.has_premium_access(query.from_user.id):
                 if clicked == typed:
-                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{ident}_{file_id}")
+                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={ident}_{file_id}")
                     return
                 else:
                     await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
 
             else:
                 if clicked == typed:
-                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{ident}_{file_id}")
+                    await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={ident}_{file_id}")
                     return
                 else:
                     await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
         except UserIsBlocked:
             await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
         except PeerIdInvalid:
-            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{ident}_{file_id}")
+            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={ident}_{file_id}")
         except Exception as e:
-            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{ident}_{file_id}")
+            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={ident}_{file_id}")
 
     elif query.data.startswith("sendfiles"):
         clicked = query.from_user.id
@@ -1394,22 +1394,22 @@ async def cb_handler(client: Client, query: CallbackQuery):
         pre = 'allfilesp' if settings['file_secure'] else 'allfiles'
         try:
             if settings['is_shortlink'] and not await db.has_premium_access(query.from_user.id):
-                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichusendfiles1_{key}")
+                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=sendfiles1_{key}")
             elif settings['is_shortlink'] and await db.has_premium_access(query.from_user.id):
-                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{pre}_{key}")
+                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={pre}_{key}")
                 return 
             else:
-                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu{pre}_{key}")
+                await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu={pre}_{key}")
 
 
 
         except UserIsBlocked:
             await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
         except PeerIdInvalid:
-            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichusendfiles3_{key}")
+            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=sendfiles3_{key}")
         except Exception as e:
             logger.exception(e)
-            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichusendfiles4_{key}")
+            await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=sendfiles4_{key}")
 
     elif query.data.startswith("unmuteme"):
         ident, userid = query.data.split("#")
@@ -1452,7 +1452,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f_caption
         if f_caption is None:
             f_caption = f"{files['file_name']}"
-        await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichufile_{file_id}")
+        await query.answer(url=f"https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=file_{file_id}")
 
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
@@ -2690,7 +2690,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         if not settings["button"]:
             cap+="<b>\n\n<u>🍿 Your Movie Files 👇</u></b>\n"
             for file in files:
-                cap += f"<b>\n📁 <a href='https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichufiles_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n</a></b>"
+                cap += f"<b>\n📁 <a href='https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n</a></b>"
     else:
         if settings["button"]:
             cap = f"<b>Tʜᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ ☞ {search}\n\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {message.from_user.mention}\n\nʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ 🗑️\n\n</b>"
@@ -2698,7 +2698,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             cap = f"<b>Tʜᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ ☞ {search}\n\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {message.from_user.mention}\n\nʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ 🗑️\n\n</b>"
             cap+="<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
-                cap += f"<b>📁 <a href='https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichufiles_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
+                cap += f"<b>📁 <a href='https://thecuriousrealm.blogspot.com/p/redirecting.html?TechnicalBichu=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
 
     if imdb and imdb.get('poster'):
         try:
